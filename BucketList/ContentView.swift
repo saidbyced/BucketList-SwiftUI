5f88be53f7ee5ea8f7a6d9a6033ff179dd8b3dc7
Chris Eadie <chris.eadie@outlook.com>
2020-08-17 10:57:17 +0100

    Initial Commit

---
+++ BucketList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BucketList
+//
+//  Created by Chris Eadie on 17/08/2020.
+//  Copyright © 2020 Chris Eadie Designs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
