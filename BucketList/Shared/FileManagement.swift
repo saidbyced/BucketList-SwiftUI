766341a508971cb56652a801c7b4b1247cc01ad2
Chris Eadie <chris.eadie@outlook.com>
2020-08-17 11:13:18 +0100

    Add file manager function and tie into view

---
+++ BucketList/Shared/FileManagement.swift
@@ -0,0 +1,19 @@
+//
+//  FileManagement.swift
+//  BucketList
+//
+//  Created by Chris Eadie on 17/08/2020.
+//  Copyright © 2020 Chris Eadie Designs. All rights reserved.
+//
+
+import Foundation
+
+struct FileManagement {
+    func getDocumentsDirectory() -> URL {
+        // find all possible documents directories for this user
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        
+        // just send back the first one, which ought to be the only one
+        return paths[0]
+    }
+}
