360118555a5243703799f8eede0d19d4fa2b9040
Chris Eadie <chris.eadie@outlook.com>
2020-08-17 13:03:41 +0100

    Add coordinator to handle map location changes

--- BucketList/MapView.swift
+++ BucketList/MapView.swift
@@ -10,13 +10,34 @@ import SwiftUI
 import MapKit
 
 struct MapView: UIViewRepresentable {
+    
     func makeUIView(context: Context) -> MKMapView {
-        MKMapView()
+        let mapView = MKMapView()
+        mapView.delegate = context.coordinator
+        
+        return mapView
     }
     
     func updateUIView(_ uiView: MKMapView, context: Context) {
         // TODO: Add view update code
     }
+    
+    class Coordinator: NSObject, MKMapViewDelegate {
+        var parent: MapView
+        
+        init(_ parent: MapView) {
+            self.parent = parent
+        }
+        
+        func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
+            print(mapView.centerCoordinate)
+        }
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
 }
 
 struct MapView_Previews: PreviewProvider {
