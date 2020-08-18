6877cd97badf9df64821abf16fae718230bbb756
Chris Eadie <chris.eadie@outlook.com>
2020-08-18 12:49:48 +0100

    Update view ready for tying into main view

--- BucketList/MapView.swift
+++ BucketList/MapView.swift
@@ -11,6 +11,8 @@ import MapKit
 
 struct MapView: UIViewRepresentable {
     
+    @Binding var centerCoordinate: CLLocationCoordinate2D
+    
     func makeUIView(context: Context) -> MKMapView {
         let mapView = MKMapView()
         mapView.delegate = context.coordinator
@@ -35,8 +37,19 @@ struct MapView: UIViewRepresentable {
     
 }
 
+extension MKPointAnnotation {
+    static var example: MKPointAnnotation {
+        let annotation = MKPointAnnotation()
+        annotation.title = "London"
+        annotation.subtitle = "Home to the 2012 Summer Olympics"
+        annotation.coordinate = CLLocationCoordinate2D(latitude: 51.5, longitude: -0.13)
+        
+        return annotation
+    }
+}
+
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        MapView()
+        MapView(centerCoordinate: .constant(MKPointAnnotation.example.coordinate))
     }
 }
