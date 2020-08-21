b655817d5a704b07733f9828fff3eb2ffb6d795d
Chris Eadie <chris.eadie@outlook.com>
2020-08-21 10:48:15 +0100

    Update view to allow location details to be seen

--- BucketList/Edit/EditView.swift
+++ BucketList/Edit/EditView.swift
@@ -14,12 +14,30 @@ struct EditView: View {
     @ObservedObject var placemark: MKPointAnnotation
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Place name", text: $placemark.wrappedTitle)
+                    TextField("Description", text: $placemark.wrappedSubtitle)
+                }
+            }
+        .navigationBarTitle("Edit place")
+        .navigationBarItems(trailing:
+            Button(
+                action: {
+                    self.presentationMode.wrappedValue.dismiss()
+                },
+                label: {
+                    Text("Done")
+                }
+            )
+        )
+        }
     }
 }
 
 struct EditView_Previews: PreviewProvider {
     static var previews: some View {
-        EditView()
+        EditView(placemark: MKPointAnnotation.example)
     }
 }
