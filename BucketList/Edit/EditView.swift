65f9147cd5c488a00cbdf738f2b0902d09892914
Chris Eadie <chris.eadie@outlook.com>
2020-08-28 12:32:47 +0100

    Add fetching wikipedia pages for nearby locations and corrected result to use correct pageid field

--- BucketList/Edit/EditView.swift
+++ BucketList/Edit/EditView.swift
@@ -20,6 +20,29 @@ struct EditView: View {
         case loading, loaded, failed
     }
     
+    func fetchNearbyPlaces() {
+        let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(placemark.coordinate.latitude)%7C\(placemark.coordinate.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
+        
+        guard let url = URL(string: urlString) else {
+            print("Bad URL: \(urlString)")
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let data = data {
+                let decoder = JSONDecoder()
+                
+                if let items = try? decoder.decode(Result.self, from: data) {
+                    self.pages = Array(items.query.pages.values)
+                    self.loadingState = .loaded
+                    return
+                }
+            }
+            
+            self.loadingState = .failed
+        }.resume()
+    }
+    
     var body: some View {
         NavigationView {
             Form {
@@ -29,7 +52,7 @@ struct EditView: View {
                 }
                 Section(header: Text("Nearby...")) {
                     if loadingState == .loaded {
-                        List(pages, id: \.pageID) { page in
+                        List(pages, id: \.pageid) { page in
                             Text(page.title)
                                 .font(.headline)
                             + Text(": ")
@@ -53,6 +76,7 @@ struct EditView: View {
                 }
             )
         )
+                .onAppear(perform: fetchNearbyPlaces)
         }
     }
 }
